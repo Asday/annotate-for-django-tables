ad6855f8041376a06cfc3c78ef05eceab9ea815a
asday <sara.and.zuka@gmail.com>
2018-01-29 15:06:22 +0000

    Gave students names.
    Makes them much easier to differentiate.

--- students/models.py
+++ students/models.py
@@ -1,8 +1,32 @@
+from random import choice, randint
+
 from django.db import models
 
 
+vowels = 'aeiou'
+consonants = 'bcdfghjklmnpqrstvwxyz'
+
+
+def generate_name():
+    length = randint(7, 20)
+    name = []
+    consonant = choice((True, False))
+    for i in range(length):
+        if consonant:
+            name.append(choice(consonants))
+        else:
+            name.append(choice(vowels))
+
+        consonant ^= True  # Flip to the opposite value.
+
+    return ''.join(name)
+
+
 class Student(models.Model):
-    pass
+    name = models.CharField(max_length=20, default=generate_name)
+
+    def __str__(self):
+        return self.name
 
 
 class AcademicStanding(models.Model):
